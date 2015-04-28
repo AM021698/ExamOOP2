29f37125c3c9f27627cbba27fcd98fd4becd2267
AM021698 <chu_tae@hotmail.com.tw>
2015-04-28 17:58:52 +0800

    B AM021698 朱泰瑞提交 editFile

--- Question2.java
+++ Question2.java
@@ -1,7 +1,36 @@
-﻿public class Question2{
+﻿import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Question2{
 	public static void main(String[] args){
 		txtCopy("Source.txt", "Dest.txt");
 		editFile("Dest.txt");
 		updateFile("Dest.txt");
 	}
+	public static void editFile(String file_name) {
+		Path path = Paths.get(file_name);
+		Charset charset = StandardCharsets.UTF_8;
+		String content = "";
+		try {
+			content = new String(Files.readAllBytes(path), charset);
+			content = content.replaceAll("a", "");
+			content = content.replaceAll("e", "");
+			content = content.replaceAll("i", "");
+			content = content.replaceAll("o", "");
+			content = content.replaceAll("u", "");
+			content = content.replaceAll("A", "");
+			content = content.replaceAll("E", "");
+			content = content.replaceAll("I", "");
+			content = content.replaceAll("O", "");
+			content = content.replaceAll("U", "");
+			Files.write(path, content.getBytes(charset));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+ 	}
 }
